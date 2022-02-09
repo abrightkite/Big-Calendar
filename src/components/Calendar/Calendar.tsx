f9c5f8fc00cc83e229726446ae0e9fce9d5a0235
Hellen <phyy5397@gmial.com>
2022-02-09 17:32:02 +0900

    setting component

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Calender = () => {
+  return <div className="calender-wrap">Calendar Component!</div>;
+};
+
+export default Calender;
---
+++ src/components/Calendar/containers/CalendarContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Calender from "../Calendar";
+
+const CalendarContainer = () => {
+  return <Calender />;
+};
+
+export default CalendarContainer;
