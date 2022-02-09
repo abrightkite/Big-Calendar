6d1c38ca4aa36f9347a3a853605bae72d9282bde
Hellen <phyy5397@gmial.com>
2022-02-09 20:28:16 +0900

    update webpack

--- src/routes/RootNavigation.tsx
+++ src/routes/RootNavigation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CalendarContainer from "../components/Calendar/containers/CalendarContainer";
+import CalendarContainer from "@components/Calendar/containers/CalendarContainer";
 
 const RootNavigation = () => {
   return (
