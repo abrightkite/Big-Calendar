2031cf39aebbd6fd84ada601db09862355f53c04
Hellen <phyy5397@gmial.com>
2022-02-09 17:48:38 +0900

    setting routes

---
+++ src/routes/RootNavigation.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CalendarContainer from "../components/Calendar/containers/CalendarContainer";
+
+const RootNavigation = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CalendarContainer />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RootNavigation;
