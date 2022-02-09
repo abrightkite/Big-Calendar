5b14a11cb9314af733db039a6c2c28365f4a7d47
Hellen <phyy5397@gmial.com>
2022-02-09 19:41:07 +0900

    store

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./store/rootReducer";
+import "./styles/core.css";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById("root")
 );
